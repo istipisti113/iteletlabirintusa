2e90ad6abbbe14282d7f4be1d19459f4cf003721
Szabó István <pisti4395@gmail.com>
2025-12-01 11:28:03 +0100

    fooldal kesznek minositheto

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function loadPage(page, id) {
       document.getElementById(id).innerHTML = data;
     });
 }
+
+async function loadPageAddClass(page, id, id2) {
+  fetch(page)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+    })
+    .then(_ => {
+      document.getElementById(id2).classList.add("nav-home")
+    })
+}
