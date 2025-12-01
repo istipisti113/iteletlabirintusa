fce7995a86f0248117d52637c541a1b00febbe12
Szabó István <pisti4395@gmail.com>
2025-12-01 09:49:43 +0100

    asdf

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,13 @@ async fn main() {
     println!("port is {}", port);
     let script = warp::path("script.js").and(warp::fs::file("script.js"));
     let css = warp::path("index.css").and(warp::fs::file("html/index.css"));
+    let szabalyokcss = warp::path("szabalyok.css").and(warp::fs::file("html/szabalyok.css"));
     let home = warp::path::end().map(|| warp::reply::html(fs::read_to_string("html/index.html").unwrap()));
     let help = warp::path::end().map(|| warp::reply::html(fs::read_to_string("html/help.html").unwrap()));
+    let header = warp::path("header").map(|| warp::reply::html(fs::read_to_string("html/header.html").unwrap()));
+    let footer = warp::path("footer").map(|| warp::reply::html(fs::read_to_string("html/footer.html").unwrap()));
     let leiras = warp::path("leiras").map(move || warp::reply::html("a".replace("a", &h3(&jatekleiras))));
-    let tortenetszoveg = warp::path("tortenetszoveg").map(move || warp::reply::html(String::from("asdf".replace("asdf", &hattertortenet
-        .replace("==============", "<br>==============<br>")))));
+    let tortenetszoveg = warp::path("tortenetszoveg").map(move || warp::reply::html(String::from("asdf".replace("asdf", &h3(&hattertortenet)))));
     let tortenet = warp::path("tortenet").map(move || warp::reply::html(fs::read_to_string("html/hattertortenet.html").unwrap()));
     let szabalyok = warp::path("szabalyok").map(||warp::reply::html(fs::read_to_string("html/szabalyok.html").unwrap()));
     let cards = warp::path!("card"/ usize).map(move |card: usize|
@@ -30,7 +32,7 @@ async fn main() {
 
 
     let routes = home.or(help).or(tortenet).or(cards).or(tortenetszoveg).or(leiras).or(szabalyok)
-    .or(script).or(css);
+    .or(script).or(css).or(szabalyokcss).or(header).or(footer);
     warp::serve(routes).run(([0,0,0,0], port)).await;
 }
 
