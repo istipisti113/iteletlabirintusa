c877fb5cb6221b6540e241fb29c3d2295e00c8f6
Szabo Istvan <pisti4395@gmail.com>
2026-01-04 12:31:31 +0100

    asfd

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,7 @@ async fn main() {
     let help = warp::path::end().map(|| warp::reply::html(fs::read_to_string("html/help.html").unwrap()));
     let header = warp::path("header").map(|| warp::reply::html(fs::read_to_string("html/header.html").unwrap()));
     let footer = warp::path("footer").map(|| warp::reply::html(fs::read_to_string("html/footer.html").unwrap()));
+    let harc = warp::path("harc").map(|| warp::reply::html(fs::read_to_string("html/harc.html").unwrap()));
     let leiras = warp::path("leiras").map(move || warp::reply::html("a".replace("a", &h3(&jatekleiras))));
     let tortenetszoveg = warp::path("tortenetszoveg").map(move || warp::reply::html(String::from("asdf".replace("asdf", &h3(&hattertortenet)))));
     let segedfile = warp::path("segedfile").map(||{
@@ -86,7 +87,7 @@ async fn main() {
         )
     });
 
-    let routes = home.or(help).or(tortenet).or(cards).or(tortenetszoveg).or(leiras).or(szabalyok).or(jatek).or(segedfile).or(jatekscript)
+    let routes = home.or(help).or(tortenet).or(cards).or(tortenetszoveg).or(leiras).or(szabalyok).or(jatek).or(segedfile).or(jatekscript).or(harc)
     .or(script).or(css).or(szabalyokcss).or(header).or(footer);
     warp::serve(routes).run(([0,0,0,0], port)).await;
 }
